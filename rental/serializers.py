756e6bf228c3c04ace5aa0a9a235c0df5aea6508
bbandm <hjerachel@gmail.com>
2024-08-23 23:26:38 +0900

    겟 오류 해결

--- rental/serializers.py
+++ rental/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
 from .models import RentalForm
+import secrets
+import string
 
 class RentalFormSerializer(serializers.ModelSerializer):
     class Meta:
         model = RentalForm
         fields = ['id', 'userName', 'phoneNumber', 'startDate', 'finishDate', 'items', 'place', 'orderNumber']
+
+    def create(self, validated_data):
+        rental_form = RentalForm.objects.create(**validated_data)
+        rental_form.orderNumber = self.generate_secure_random_string()
+        rental_form.save()
+        return rental_form
+
+    def generate_secure_random_string(self, length=8):
+        characters = string.ascii_letters + string.digits
+        return ''.join(secrets.choice(characters) for _ in range(length))
+    
