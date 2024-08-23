e086bd1f443052fa39f6f324a1344f2de555391c
psb <psb3707@naver.com>
2024-08-23 22:49:40 +0900

    serializer init

--- rental/apps.py
+++ rental/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LentalConfig(AppConfig):
+class RentalConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lental'
+    name = 'rental'
