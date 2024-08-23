632c5f712ffc2417eddc2d8a7ee9af9ed4d9672e
psb <psb3707@naver.com>
2024-08-23 23:18:12 +0900

    get method init

--- rental/views.py
+++ rental/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from .serializers import RentalFormSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from .models import RentalForm
 
 @api_view(['POST'])
 def create_rentalform(request):
@@ -13,3 +14,15 @@ def create_rentalform(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     else:
         return Response("Invalid Data", status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def get_rentalform(request):
+    query = request.GET.get('orderNumber',None)
+    if query is None:
+        return Response("주문번호가 필요합니다.", status=status.HTTP_400_BAD_REQUEST)
+    rentalForm = RentalForm.objects.get(orderNumber=query)
+    if rentalForm.DoesNotExist:
+        return Response("주문번호에 해당하는 대여기록이 존재하지 않습니다.",status=status.HTTP_404_NOT_FOUND)
+    else:
+        serializer = RentalFormSerializer(instance=rentalForm)
+        return Response(serializer.data, status=status.HTTP_200_OK)
